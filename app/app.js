7c7f293a3c10ca200ef237916f993581ae78888f
Ziwang_Li <ujunglim@naver.com>
2021-09-02 10:33:58 +0800

    add front js, express.static()

--- app/app.js
+++ app/app.js
@@ -11,7 +11,9 @@ const home = require('./src/routes/home');
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+// middleware
+// 정적경로 추가  __dirname: app.js가 있는 위치반환 
+app.use(express.static(`${__dirname}/src/public`));
 app.use("/", home); // 루트경로로 들어오면 홈으로 보내줌.
-// use는 middleware를 등록하는 메소드
 
 module.exports = app;
