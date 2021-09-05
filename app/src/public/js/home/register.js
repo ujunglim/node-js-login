53eeb446635bc713d6d6bba5fb5ac81565585556
Yuyu <ujunglim@naver.com>
2021-09-05 21:30:34 +0800

    add register

---
+++ app/src/public/js/home/register.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const id = document.querySelector('#id');
+const name = document.querySelector('#name');
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirm-password');
+const registerBTN = document.querySelector('button');
+
+registerBTN.addEventListener('click', () => {
+
+	// // get req
+	// const req = {
+	// 	id: id.value,
+  //   name: name.value,
+	// 	password: password.value,
+  //   confirmPassword: confirmPassword.value,
+	// };
+
+	// // 서버에 req 전달, fetch(경로, 전달하는 데이터)
+	// fetch('/register', {
+	// 	method: 'POST', // body를 통해 보낼때는 http 메서드 중에서 POST 메서드로 전달
+	// 	headers: {
+	// 		"Content-Type": "application/json", // 전달하는 데이터 타입 = json
+	// 	},
+	// 	body: JSON.stringify(req),
+	// })
+	// // 서버에서 응답한 데이터 받기
+	// .then((res) => res.json()) // 반환값 전달
+	// .then((res) => {
+	// 	if(res.success) {
+	// 		location.href = '/'; // 이동
+	// 	}
+	// 	else {
+	// 		alert(res.msg);
+	// 	}
+	// })
+	// .catch((err) => {
+	// 	console.error(new Error('Error to register'));
+	// })
+})
