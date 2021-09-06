52d7bfbab5bb18bef5afff9f8ee65c14aa1b9b1f
Ziwang_Li <ujunglim@naver.com>
2021-09-06 15:20:56 +0800

    register routing

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -8,14 +8,14 @@ class User {
   }
 
   login() {
-    const body = this.body;
+    const client = this.body;
     // get data from UserStorage
-    const {id, password} = UserStorage.getUserInfo(body.id);
+    const {id, password} = UserStorage.getUserInfo(client.id);
     
     // id가 storage에 있는지 체크
     if(id) {
       // storage값과 client가 입력한 값이 같을시   
-      if(id === body.id && password === body.password) {
+      if(id === client.id && password === client.password) {
         return {success: true};
       } 
       return {
@@ -27,7 +27,13 @@ class User {
       success: false,
       msg: 'id is not exist'
     }
-   
+  }
+
+  register() {
+    const client = this.body;
+    // save client info to UserStorage
+    const response = UserStorage.save(client);
+    return response;
   }
 
 }
--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -6,35 +6,39 @@ const password = document.querySelector('#password');
 const confirmPassword = document.querySelector('#confirm-password');
 const registerBTN = document.querySelector('button');
 
-registerBTN.addEventListener('click', () => {
+registerBTN.addEventListener('click', (evt) => {
+	evt.preventDefault();
 
-	// // get req
-	// const req = {
-	// 	id: id.value,
-  //   name: name.value,
-	// 	password: password.value,
-  //   confirmPassword: confirmPassword.value,
-	// };
+	if(!id.value) return alert('Please input Username');
+	// check password 
+	if(password.value !== confirmPassword.value) return alert('Passwords do not match')
 
-	// // 서버에 req 전달, fetch(경로, 전달하는 데이터)
-	// fetch('/register', {
-	// 	method: 'POST', // body를 통해 보낼때는 http 메서드 중에서 POST 메서드로 전달
-	// 	headers: {
-	// 		"Content-Type": "application/json", // 전달하는 데이터 타입 = json
-	// 	},
-	// 	body: JSON.stringify(req),
-	// })
-	// // 서버에서 응답한 데이터 받기
-	// .then((res) => res.json()) // 반환값 전달
-	// .then((res) => {
-	// 	if(res.success) {
-	// 		location.href = '/'; // 이동
-	// 	}
-	// 	else {
-	// 		alert(res.msg);
-	// 	}
-	// })
-	// .catch((err) => {
-	// 	console.error(new Error('Error to register'));
-	// })
+	// get req
+	const req = {
+		id: id.value,
+		name: name.value,
+		password: password.value,
+	};
+
+	// 서버에 req 전달, fetch(경로, 전달하는 데이터)
+	fetch('/register', {
+		method: 'POST', // body를 통해 보낼때는 http 메서드 중에서 POST 메서드로 전달
+		headers: {
+			"Content-Type": "application/json", // 전달하는 데이터 타입 = json
+		},
+		body: JSON.stringify(req),
+	})
+	// 서버에서 응답한 데이터 받기
+	.then((res) => res.json()) // 반환값 promise 전달
+	.then((res) => {
+		if(res.success) {
+			location.href = '/login'; // 이동
+		}
+		else {
+			alert(res.msg);
+		}
+	})
+	.catch((err) => {
+		console.error(new Error('Error to register'));
+	})
 })
